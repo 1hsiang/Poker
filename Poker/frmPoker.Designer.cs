599da2c5f212e27151460ea892528715f06888a5
1hsiang <ethanhsiao0604@gmail.com>
2026-05-06 16:17:33 +0800

    加入專案檔案。

---
+++ Poker/frmPoker.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Poker
+{
+    partial class frmPoker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpPoker = new System.Windows.Forms.GroupBox();
+            this.grpButton = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnChangeCard = new System.Windows.Forms.Button();
+            this.btnDealCard = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBet = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBet = new System.Windows.Forms.ComboBox();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.grpButton.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpPoker
+            // 
+            this.grpPoker.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpPoker.Location = new System.Drawing.Point(16, 15);
+            this.grpPoker.Margin = new System.Windows.Forms.Padding(4);
+            this.grpPoker.Name = "grpPoker";
+            this.grpPoker.Padding = new System.Windows.Forms.Padding(4);
+            this.grpPoker.Size = new System.Drawing.Size(695, 185);
+            this.grpPoker.TabIndex = 0;
+            this.grpPoker.TabStop = false;
+            this.grpPoker.Text = "牌桌";
+            // 
+            // grpButton
+            // 
+            this.grpButton.Controls.Add(this.lblResult);
+            this.grpButton.Controls.Add(this.btnCheck);
+            this.grpButton.Controls.Add(this.btnChangeCard);
+            this.grpButton.Controls.Add(this.btnDealCard);
+            this.grpButton.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpButton.Location = new System.Drawing.Point(16, 297);
+            this.grpButton.Margin = new System.Windows.Forms.Padding(4);
+            this.grpButton.Name = "grpButton";
+            this.grpButton.Padding = new System.Windows.Forms.Padding(4);
+            this.grpButton.Size = new System.Drawing.Size(703, 100);
+            this.grpButton.TabIndex = 1;
+            this.grpButton.TabStop = false;
+            this.grpButton.Text = "功能";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(281, 19);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(414, 77);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "\r\n\r\n                                                                             " +
+    "                    ";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCheck.Enabled = false;
+            this.btnCheck.Location = new System.Drawing.Point(172, 35);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(101, 39);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "判斷牌型";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnChangeCard
+            // 
+            this.btnChangeCard.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnChangeCard.Enabled = false;
+            this.btnChangeCard.Location = new System.Drawing.Point(86, 35);
+            this.btnChangeCard.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChangeCard.Name = "btnChangeCard";
+            this.btnChangeCard.Size = new System.Drawing.Size(78, 39);
+            this.btnChangeCard.TabIndex = 1;
+            this.btnChangeCard.Text = "換牌";
+            this.btnChangeCard.UseVisualStyleBackColor = false;
+            this.btnChangeCard.Click += new System.EventHandler(this.btnChangeCard_Click);
+            // 
+            // btnDealCard
+            // 
+            this.btnDealCard.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDealCard.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnDealCard.Location = new System.Drawing.Point(8, 35);
+            this.btnDealCard.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDealCard.Name = "btnDealCard";
+            this.btnDealCard.Size = new System.Drawing.Size(70, 39);
+            this.btnDealCard.TabIndex = 0;
+            this.btnDealCard.Text = "發牌";
+            this.btnDealCard.UseVisualStyleBackColor = false;
+            this.btnDealCard.Click += new System.EventHandler(this.btnDealCard_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "總資金";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBet);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cmbBet);
+            this.groupBox1.Controls.Add(this.cmbRole);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 207);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(695, 74);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnBet
+            // 
+            this.btnBet.Location = new System.Drawing.Point(549, 24);
+            this.btnBet.Name = "btnBet";
+            this.btnBet.Size = new System.Drawing.Size(84, 37);
+            this.btnBet.TabIndex = 6;
+            this.btnBet.Text = "押注";
+            this.btnBet.UseVisualStyleBackColor = true;
+            this.btnBet.Click += new System.EventHandler(this.btnBet_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(292, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "押注金額";
+            // 
+            // cmbBet
+            // 
+            this.cmbBet.FormattingEnabled = true;
+            this.cmbBet.Location = new System.Drawing.Point(375, 29);
+            this.cmbBet.Name = "cmbBet";
+            this.cmbBet.Size = new System.Drawing.Size(136, 23);
+            this.cmbBet.TabIndex = 4;
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(86, 30);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(179, 23);
+            this.cmbRole.TabIndex = 3;
+            // 
+            // frmPoker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 433);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grpButton);
+            this.Controls.Add(this.grpPoker);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmPoker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "五張撲克牌";
+            this.grpButton.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpPoker;
+        private System.Windows.Forms.GroupBox grpButton;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnChangeCard;
+        private System.Windows.Forms.Button btnDealCard;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbBet;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.Button btnBet;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Poker/frmPoker.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poker
+{
+    public partial class frmPoker : Form
+    {
+        PictureBox[] pic = new PictureBox[5];
+
+        int[] allPoker = new int[52];
+        int[] playerPoker = new int[5];
+
+        int totalMoney = 0;
+        int currentBet = 0;
+        bool gameStarted = false;
+
+        Random rand = new Random();
+
+        public frmPoker()
+        {
+            InitializeComponent();
+
+            InitializePoker();
+            InitializeBetSystem();
+            BindEvents();
+        }
+
+        private void InitializePoker()
+        {
+            grpPoker.Controls.Clear();
+
+            for (int i = 0; i < pic.Length; i++)
+            {
+                pic[i] = new PictureBox();
+
+                pic[i].Name = "pic" + i;
+                pic[i].Image = GetImage("back");
+                pic[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                pic[i].Width = 80;
+                pic[i].Height = 110;
+                pic[i].Top = 30;
+                pic[i].Left = 25 + ((pic[i].Width + 25) * i);
+
+                pic[i].Enabled = false;
+                pic[i].Tag = "front";
+                pic[i].BorderStyle = BorderStyle.None;
+                pic[i].Visible = true;
+
+                grpPoker.Controls.Add(pic[i]);
+            }
+        }
+
+        private void InitializeBetSystem()
+        {
+            cmbRole.Items.Clear();
+            cmbRole.Items.Add("富二代");
+            cmbRole.Items.Add("凡人");
+            cmbRole.Items.Add("窮人");
+            cmbRole.SelectedIndex = 1;
+
+            cmbBet.Items.Clear();
+            cmbBet.Items.Add("100");
+            cmbBet.Items.Add("500");
+            cmbBet.Items.Add("1000");
+            cmbBet.Items.Add("5000");
+            cmbBet.Text = "500";
+
+            btnDealCard.Enabled = false;
+            btnChangeCard.Enabled = false;
+            btnCheck.Enabled = false;
+
+            lblResult.Text = "請選擇出生背景並下注";
+
+            this.AcceptButton = btnBet;
+        }
+
+        private void BindEvents()
+        {
+            cmbRole.SelectedIndexChanged -= cmbRole_SelectedIndexChanged;
+            cmbRole.SelectedIndexChanged += cmbRole_SelectedIndexChanged;
+
+            btnBet.Click -= btnBet_Click;
+            btnBet.Click += btnBet_Click;
+
+            btnDealCard.Click -= btnDealCard_Click;
+            btnDealCard.Click += btnDealCard_Click;
+
+            btnChangeCard.Click -= btnChangeCard_Click;
+            btnChangeCard.Click += btnChangeCard_Click;
+
+            btnCheck.Click -= btnCheck_Click;
+            btnCheck.Click += btnCheck_Click;
+
+            for (int i = 0; i < pic.Length; i++)
+            {
+                pic[i].Click -= Pic_Click;
+                pic[i].Click += Pic_Click;
+            }
+        }
+
+        private void cmbRole_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (gameStarted) return;
+
+            switch (cmbRole.Text)
+            {
+                case "富二代":
+                    totalMoney = 1000000;
+                    lblResult.Text = "出生背景：富二代，目前資金：1000000";
+                    break;
+
+                case "凡人":
+                    totalMoney = 100000;
+                    lblResult.Text = "出生背景：凡人，目前資金：100000";
+                    break;
+
+                case "窮人":
+                    totalMoney = 10000;
+                    lblResult.Text = "出生背景：窮人，目前資金：10000";
+                    break;
+            }
+        }
+
+        private void btnBet_Click(object sender, EventArgs e)
+        {
+            int bet;
+
+            if (!int.TryParse(cmbBet.Text, out bet))
+            {
+                lblResult.Text = "請輸入正確的押注金額";
+                return;
+            }
+
+            if (bet <= 0)
+            {
+                lblResult.Text = "押注金額必須大於 0";
+                return;
+            }
+
+            if (bet > totalMoney)
+            {
+                lblResult.Text = "資金不足，請降低押注金額";
+                return;
+            }
+
+            currentBet = bet;
+            totalMoney -= bet;
+
+            gameStarted = true;
+            cmbRole.Enabled = false;
+
+            btnDealCard.Enabled = true;
+            btnChangeCard.Enabled = false;
+            btnCheck.Enabled = false;
+
+            lblResult.Text = "下注成功：" + bet +
+                             "，目前資金：" + totalMoney +
+                             "，請按「發牌」";
+        }
+
+        private async void btnDealCard_Click(object sender, EventArgs e)
+        {
+            if (currentBet <= 0)
+            {
+                lblResult.Text = "請先下注";
+                return;
+            }
+
+            lblResult.Text = "發牌中...";
+
+            for (int i = 0; i < pic.Length; i++)
+            {
+                pic[i].Image = GetImage("back");
+                pic[i].Enabled = false;
+                pic[i].Tag = "front";
+                pic[i].BorderStyle = BorderStyle.None;
+            }
+
+            for (int i = 0; i < allPoker.Length; i++)
+            {
+                allPoker[i] = i;
+            }
+
+            Shuffle();
+
+            await Task.Delay(300);
+
+            for (int i = 0; i < playerPoker.Length; i++)
+            {
+                playerPoker[i] = allPoker[i];
+            }
+
+            ShowCards();
+
+            for (int i = 0; i < pic.Length; i++)
+            {
+                pic[i].Enabled = true;
+                pic[i].Tag = "front";
+                pic[i].BorderStyle = BorderStyle.None;
+            }
+
+            btnDealCard.Enabled = false;
+            btnChangeCard.Enabled = true;
+            btnCheck.Enabled = false;
+
+            lblResult.Text = "已發牌，請點選想換掉的牌，再按「換牌」";
+        }
+
+        private void Pic_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPic = sender as PictureBox;
+
+            int index = Array.IndexOf(pic, clickedPic);
+            if (index < 0) return;
+
+            int cardNum = playerPoker[index] + 1;
+
+            if (clickedPic.Tag.ToString() == "front")
+            {
+                clickedPic.Tag = "back";
+                clickedPic.Image = GetImage("back");
+                clickedPic.BorderStyle = BorderStyle.Fixed3D;
+                lblResult.Text = "已選擇第 " + (index + 1) + " 張牌要換";
+            }
+            else
+            {
+                clickedPic.Tag = "front";
+                clickedPic.Image = GetImage(cardNum);
+                clickedPic.BorderStyle = BorderStyle.None;
+                lblResult.Text = "取消選擇第 " + (index + 1) + " 張牌";
+            }
+        }
+
+        private void btnChangeCard_Click(object sender, EventArgs e)
+        {
+            int startIndex = 5;
+            int changeCount = 0;
+
+            for (int i = 0; i < playerPoker.Length; i++)
+            {
+                if (pic[i].Tag.ToString() == "back")
+                {
+                    playerPoker[i] = allPoker[startIndex];
+                    startIndex++;
+                    changeCount++;
+                }
+            }
+
+            ShowCards();
+
+            for (int i = 0; i < pic.Length; i++)
+            {
+                pic[i].Enabled = false;
+                pic[i].Tag = "front";
+                pic[i].BorderStyle = BorderStyle.None;
+            }
+
+            btnChangeCard.Enabled = false;
+            btnCheck.Enabled = true;
+
+            lblResult.Text = "換牌完成，共換了 " + changeCount + " 張，請按「判斷牌型」";
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string result = CheckHand();
+            int multiplier = GetMultiplier(result);
+            int winMoney = currentBet * multiplier;
+
+            totalMoney += winMoney;
+
+            lblResult.Text = "牌型：" + result +
+                             "，賠率：" + multiplier +
+                             "，中獎金額：" + winMoney +
+                             "，目前資金：" + totalMoney;
+
+            currentBet = 0;
+
+            btnDealCard.Enabled = false;
+            btnChangeCard.Enabled = false;
+            btnCheck.Enabled = false;
+
+            if (totalMoney <= 0)
+            {
+                lblResult.Text += "。你破產了，請重新選擇出生背景。";
+                gameStarted = false;
+                cmbRole.Enabled = true;
+            }
+            else
+            {
+                lblResult.Text += "。請重新下注開始下一局。";
+            }
+        }
+
+        private void Shuffle()
+        {
+            for (int i = 0; i < allPoker.Length; i++)
+            {
+                int r = rand.Next(allPoker.Length);
+
+                int temp = allPoker[i];
+                allPoker[i] = allPoker[r];
+                allPoker[r] = temp;
+            }
+        }
+
+        private void ShowCards()
+        {
+            for (int i = 0; i < playerPoker.Length; i++)
+            {
+                pic[i].Image = GetImage(playerPoker[i] + 1);
+            }
+        }
+
+        private Image GetImage(string name)
+        {
+            return Properties.Resources.ResourceManager.GetObject(name) as Image;
+        }
+
+        private Image GetImage(int num)
+        {
+            return GetImage("pic" + num);
+        }
+
+        private string CheckHand()
+        {
+            int[] pokerColor = new int[5];
+            int[] pokerPoint = new int[5];
+
+            for (int i = 0; i < playerPoker.Length; i++)
+            {
+                pokerColor[i] = playerPoker[i] % 4;
+                pokerPoint[i] = playerPoker[i] / 4;
+            }
+
+            int[] colorCount = new int[4];
+            int[] pointCount = new int[13];
+
+            for (int i = 0; i < 5; i++)
+            {
+                colorCount[pokerColor[i]]++;
+                pointCount[pokerPoint[i]]++;
+            }
+
+            Array.Sort(pointCount);
+            Array.Reverse(pointCount);
+
+            bool isFlush = colorCount.Any(c => c == 5);
+
+            int[] sortedPoint = pokerPoint.OrderBy(x => x).ToArray();
+
+            bool isNormalStraight = true;
+
+            for (int i = 0; i < sortedPoint.Length - 1; i++)
+            {
+                if (sortedPoint[i + 1] - sortedPoint[i] != 1)
+                {
+                    isNormalStraight = false;
+                    break;
+                }
+            }
+
+            bool isRoyal = pokerPoint.Contains(0) &&
+                           pokerPoint.Contains(9) &&
+                           pokerPoint.Contains(10) &&
+                           pokerPoint.Contains(11) &&
+                           pokerPoint.Contains(12);
+
+            bool isStraight = isNormalStraight || isRoyal;
+
+            if (isRoyal && isFlush)
+                return "皇家同花順";
+            if (isStraight && isFlush)
+                return "同花順";
+            if (pointCount[0] == 4)
+                return "四條";
+            if (pointCount[0] == 3 && pointCount[1] == 2)
+                return "葫蘆";
+            if (isFlush)
+                return "同花";
+            if (isStraight)
+                return "順子";
+            if (pointCount[0] == 3)
+                return "三條";
+            if (pointCount[0] == 2 && pointCount[1] == 2)
+                return "兩對";
+            if (pointCount[0] == 2)
+                return "一對";
+
+            return "無";
+        }
+
+        private int GetMultiplier(string type)
+        {
+            switch (type)
+            {
+                case "皇家同花順": return 250;
+                case "同花順": return 50;
+                case "四條": return 25;
+                case "葫蘆": return 9;
+                case "同花": return 6;
+                case "順子": return 4;
+                case "三條": return 3;
+                case "兩對": return 2;
+                case "一對": return 1;
+                default: return 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnBet_Click(sender, e);
+        }
+    }
+}
